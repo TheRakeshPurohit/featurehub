f3a4d41e1d041af9a846798c6fa2c0ae4b385a16
Richard Vowles <richard@bluetrainsoftware.com>
2022-03-09 21:27:30 +1300

    might be working

--- pipeline/app-e2e-pipeline/build.sh
+++ pipeline/app-e2e-pipeline/build.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cloud-build-local --substitutions _DOCKER_PREFIX=bluetrainsoftware.lan:5000/featurehub,_DOCKERHUB_PASSWORD=$FEATUREHUB_PASSWORD  --config=pipeline/app-e2e-pipeline/cloudbuild.yaml --dryrun=false --write-workspace=/tmp/cloud-build .
+cd $(git rev-parse --show-toplevel)
+cloud-build-local --substitutions _DOCKER_PREFIX=bluetrainsoftware.lan/featurehub,_DOCKER_PASSWORD=$FEATUREHUB_PASSWORD,_DOCKER_USERNAME=$FEATUREHUB_USER  --config=pipeline/app-e2e-pipeline/cloudbuild.yaml --dryrun=false --write-workspace=/tmp/cloud-build .
